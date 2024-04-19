779458cd4b40bacbe4d6b58bfa94ad891c1cca5e
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-19 17:44:01 -0300

    3 posições de injeção: construtor, setter e atributo

--- src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
+++ src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
@@ -2,19 +2,31 @@ package org.pedroamorim.projetobootcamp.service;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
 import org.pedroamorim.projetobootcamp.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
+
     private Notificador notificador;
 
+    @Autowired
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
     }
+//
+//    public AtivacaoClienteService(String qualquer){
+//
+//    }
 
     public void ativar(Cliente cliente){
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
+//    @Autowired
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 }
