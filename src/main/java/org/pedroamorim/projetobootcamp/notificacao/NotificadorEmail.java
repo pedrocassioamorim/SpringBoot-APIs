49533a4080c4c7d32cbb011b9a93a36dcbd34ad8
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-19 15:38:53 -0300

    Definindo Beans Spring

--- src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
+++ src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
@@ -1,8 +1,11 @@
 package org.pedroamorim.projetobootcamp.notificacao;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NotificadorEmail {
+
     public void notificar (Cliente cliente, String mensagem){
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
