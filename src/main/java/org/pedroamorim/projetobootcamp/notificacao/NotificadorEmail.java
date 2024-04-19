4db253038f38cf35dbb8a5e49e033a9d9ed1bbbc
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-19 17:28:33 -0300

    Limpando classes para entender pontos de injeção

--- src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
+++ src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
@@ -1,28 +1,15 @@
 package org.pedroamorim.projetobootcamp.notificacao;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-
-    private boolean caixaAlta;
-    private String hostServidorSMTP;
-
-    public NotificadorEmail(String hostServidorSMTP) {
-        this.hostServidorSMTP = hostServidorSMTP;
-        System.out.println("Notificador Email");
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        if (this.caixaAlta){
-            mensagem = mensagem.toUpperCase();
-        }
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
-    }
 }
--- src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
+++ src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
@@ -2,14 +2,14 @@ package org.pedroamorim.projetobootcamp.service;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
 import org.pedroamorim.projetobootcamp.notificacao.Notificador;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class AtivacaoClienteService {
     private Notificador notificador;
 
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
-        System.out.println("AtivacaoClienteService: " + notificador);
     }
 
     public void ativar(Cliente cliente){
