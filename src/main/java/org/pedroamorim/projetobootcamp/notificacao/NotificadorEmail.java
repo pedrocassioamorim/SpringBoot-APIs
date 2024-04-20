ae06610243ecb4ebb2eb10382580f370651b1539
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-20 19:49:49 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
+++ src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package org.pedroamorim.projetobootcamp.notificacao;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     public NotificadorEmail(){
         System.out.println("Noficador de E-mail Real");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
--- src/main/java/org/pedroamorim/projetobootcamp/ouvinte/NotificacaoService.java
+++ src/main/java/org/pedroamorim/projetobootcamp/ouvinte/NotificacaoService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class NotificacaoService {
-    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
