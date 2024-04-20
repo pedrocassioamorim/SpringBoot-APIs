a3983a2601cb085c4fdac3c96832314f7b94b815
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-20 09:28:40 -0300

    Desambiguação de Beans usando @Primary

--- src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorSMS.java
+++ src/main/java/org/pedroamorim/projetobootcamp/notificacao/NotificadorSMS.java
@@ -1,8 +1,10 @@
 package org.pedroamorim.projetobootcamp.notificacao;
 
 import org.pedroamorim.projetobootcamp.modelo.Cliente;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class NotificadorSMS implements Notificador {
 
--- src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
+++ src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
@@ -5,24 +5,19 @@ import org.pedroamorim.projetobootcamp.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private List<Notificador> notificadores;
-
+    @Autowired
+    private Notificador notificador;
 
 
     public void ativar(Cliente cliente){
         cliente.ativar();
-        for(Notificador notificador : notificadores) {
-            if (notificador != null) {
-                notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-            } else {
-                System.out.println("Não existe Notificador, mas o cliente foi ativado");
-            }
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        } else {
+            System.out.println("Não existe Notificador, mas o cliente foi ativado");
         }
     }
 
