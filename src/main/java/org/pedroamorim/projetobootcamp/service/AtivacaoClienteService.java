95f46cb098a8072f3d1ca45526575cba8aa467da
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-20 09:24:29 -0300

    Implementando Lista de Beans: (NotificadorSMS e NotificadorEmail)

--- src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
+++ src/main/java/org/pedroamorim/projetobootcamp/service/AtivacaoClienteService.java
@@ -5,24 +5,25 @@ import org.pedroamorim.projetobootcamp.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
     @Autowired(required = false)
-    private Notificador notificador;
+    private List<Notificador> notificadores;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
+
 
     public void ativar(Cliente cliente){
         cliente.ativar();
-        if (notificador != null){
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }else {
-            System.out.println("Não existe Notificador, mas o cliente foi ativado");
+        for(Notificador notificador : notificadores) {
+            if (notificador != null) {
+                notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+            } else {
+                System.out.println("Não existe Notificador, mas o cliente foi ativado");
+            }
         }
-
     }
 
 }
