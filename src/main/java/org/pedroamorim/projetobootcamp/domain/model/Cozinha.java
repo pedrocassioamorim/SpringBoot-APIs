30d25fe8661ec133b537f8b0cd6c4cb0cd674f0b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-03 16:18:41 -0300

    Conhecendo o Lombok

--- src/main/java/org/pedroamorim/projetobootcamp/domain/model/Cozinha.java
+++ src/main/java/org/pedroamorim/projetobootcamp/domain/model/Cozinha.java
@@ -1,53 +1,23 @@
 package org.pedroamorim.projetobootcamp.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
 
-    public Cozinha(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Cozinha() {}
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- src/main/java/org/pedroamorim/projetobootcamp/domain/model/Restaurante.java
+++ src/main/java/org/pedroamorim/projetobootcamp/domain/model/Restaurante.java
@@ -1,15 +1,20 @@
 package org.pedroamorim.projetobootcamp.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante{
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     private String nome;
@@ -17,47 +22,5 @@ public class Restaurante{
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
 
-    public Restaurante(String nome, BigDecimal taxaFrete) {
-        this.nome = nome;
-        this.taxaFrete = taxaFrete;
-    }
 
-    public Restaurante() {}
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaFrete() {
-        return taxaFrete;
-    }
-
-    public void setTaxaFrete(BigDecimal taxaFrete) {
-        this.taxaFrete = taxaFrete;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
