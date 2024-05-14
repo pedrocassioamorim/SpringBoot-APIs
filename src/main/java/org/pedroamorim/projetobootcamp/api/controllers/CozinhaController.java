fca46627e0eb717d6c917b7d94912ab3b06d1953
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-14 17:45:30 -0300

    Metodo POST de CozinhaController

--- src/main/java/org/pedroamorim/projetobootcamp/api/controllers/CozinhaController.java
+++ src/main/java/org/pedroamorim/projetobootcamp/api/controllers/CozinhaController.java
@@ -5,12 +5,8 @@ import org.pedroamorim.projetobootcamp.domain.model.Cozinha;
 import org.pedroamorim.projetobootcamp.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +16,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository repository;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public List<Cozinha> listar(){
         return repository.listar();
     }
@@ -34,6 +30,11 @@ public class CozinhaController {
         else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @PostMapping @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+        return repository.salvar(cozinha);
     }
 
 
