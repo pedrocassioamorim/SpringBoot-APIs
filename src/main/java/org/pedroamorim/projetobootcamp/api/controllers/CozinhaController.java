643ffaee49065fec95589b6c36e33d80b0cf833d
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-15 11:00:38 -0300

    Metodo PUT e DELETE de CozinhaController

--- src/main/java/org/pedroamorim/projetobootcamp/api/controllers/CozinhaController.java
+++ src/main/java/org/pedroamorim/projetobootcamp/api/controllers/CozinhaController.java
@@ -1,9 +1,10 @@
 package org.pedroamorim.projetobootcamp.api.controllers;
 
-
 import org.pedroamorim.projetobootcamp.domain.model.Cozinha;
 import org.pedroamorim.projetobootcamp.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +36,35 @@ public class CozinhaController {
     @PostMapping @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha){
         return repository.salvar(cozinha);
+    }
+
+    @PutMapping("/{Id}")
+    public ResponseEntity<Cozinha> atualizar(@RequestBody Cozinha cozinha, @PathVariable Long Id){
+        Cozinha cozinhaAtual = repository.buscar(Id);
+
+        if (cozinhaAtual != null){
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            repository.salvar(cozinhaAtual);
+            return ResponseEntity.ok().body(cozinhaAtual);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/{Id}")
+    public ResponseEntity<Cozinha> remover (@PathVariable Long Id){
+        try {
+            Cozinha cozinhaExluir = repository.buscar(Id);
+            if (cozinhaExluir != null) {
+                repository.remover(Id);
+                return ResponseEntity.noContent().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (DataIntegrityViolationException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
 
