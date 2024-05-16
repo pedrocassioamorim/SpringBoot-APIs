474f2e09874642b2013e60384fec643a0ca8c67c
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-16 18:29:13 -0300

    Creating exceptions to the Service layer

---
+++ src/main/java/org/pedroamorim/projetobootcamp/domain/exceptions/EntidadeEmUsoException.java
@@ -0,0 +1,11 @@
+package org.pedroamorim.projetobootcamp.domain.exceptions;
+
+public class EntidadeEmUsoException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeEmUsoException(String mensagem){
+        super(mensagem);
+    }
+
+}
