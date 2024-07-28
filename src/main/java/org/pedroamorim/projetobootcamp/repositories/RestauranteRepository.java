0a1e94dc4fd749a3e78af7225d1239bf1c459cb1
pedrocassio <amorimpedrocassio@gmail.com>
2024-07-28 18:11:30 -0300

    Implementing Query methods inside orm.xml files with raw JPQL

--- src/main/java/org/pedroamorim/projetobootcamp/repositories/RestauranteRepository.java
+++ src/main/java/org/pedroamorim/projetobootcamp/repositories/RestauranteRepository.java
@@ -2,7 +2,6 @@ package org.pedroamorim.projetobootcamp.repositories;
 
 import org.pedroamorim.projetobootcamp.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -19,7 +18,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
 //    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     List<Restaurante> consultarPorNome(@Param("nome") String nome, @Param("id") Long cozinhaId);
 
     Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
