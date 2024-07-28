69e1ec624f1da70d44082e7ca77e6535f77c64c7
pedrocassio <amorimpedrocassio@gmail.com>
2024-07-28 19:55:15 -0300

    Implementing Dynamic Criteria API Queries

--- src/main/java/org/pedroamorim/projetobootcamp/repositories/impl/RestauranteRepositoryImpl.java
+++ src/main/java/org/pedroamorim/projetobootcamp/repositories/impl/RestauranteRepositoryImpl.java
@@ -1,7 +1,6 @@
 package org.pedroamorim.projetobootcamp.repositories.impl;
 
 
-
 import org.pedroamorim.projetobootcamp.domain.model.Restaurante;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -9,8 +8,12 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,33 +25,62 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> consultaNomeEFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
+//        IMPLEMENTAÇÃO DE CONSULTA DINAMICA
+//        var jpql = new StringBuilder();
+//        jpql.append("from Restaurante where 0 = 0 ");
+//
+//        var parametros = new HashMap<String, Object>();
+//
+//        if (StringUtils.hasLength(nome)) {
+//            jpql.append("and nome like :nome ");
+//            parametros.put("nome", "%" + nome + "%");
+//        }
+//
+//        if (taxaFreteInicial != null) {
+//            jpql.append("and taxaFrete >= :taxaFreteInicial ");
+//            parametros.put("taxaFreteInicial", taxaFreteInicial);
+//        }
+//
+//        if (taxaFreteFinal != null) {
+//            jpql.append("and taxaFrete <= :taxaFreteFinal ");
+//            parametros.put("taxaFreteFinal", taxaFreteFinal);
+//        }
+//       TypedQuery<Restaurante> query =  manager.createQuery(jpql.toString(), Restaurante.class);
+//        parametros.forEach(query::setParameter);
+//        return query.getResultList();
+// ====================================================================================================================
+//
+//        IMPLEMENTAÇÃO DE CONSULTAS UTILIZANDO CRITERIA API
 
-        var parametros = new HashMap<String, Object>();
+        // CRITERIA BUILDER - FABRICA DE CRITERIAS
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
+        // CONSTRUTOR DE CLUSULAS
+        CriteriaQuery<Restaurante> criteriaQuery = builder.createQuery(Restaurante.class);
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class); // from Restaurante
+
+        var predicates = new ArrayList<Predicate>();
+
+        if (StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
         }
 
-        if (taxaFreteInicial != null) {
-            jpql.append("and taxaFrete >= :taxaFreteInicial ");
-            parametros.put("taxaFreteInicial", taxaFreteInicial);
+        if (taxaFreteInicial != null){
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
         }
-
-        if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFreteFinal ");
-            parametros.put("taxaFreteFinal", taxaFreteFinal);
+        if (taxaFreteFinal != null){
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
         }
 
 
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
 
-       TypedQuery<Restaurante> query =  manager.createQuery(jpql.toString(), Restaurante.class);
 
-
-        parametros.forEach(query::setParameter);
+        // TRANSFORMANDO AS CLAUSULAS PARA UMA QUERY
+        TypedQuery<Restaurante> query = manager.createQuery(criteriaQuery);
         return query.getResultList();
+
+
     }
 
 
